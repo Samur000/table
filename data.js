b071bb39814987d749914313d6ff71eb50249600
Samur Gafurov <107584285+Samur000@users.noreply.github.com>
2025-04-17 11:02:17 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,81 @@
 const data = [
     {
+        weekDay: 'Понедельник',
+        classes: [
+            {
+                id: 1,
+                title: 'Web разработка',
+                teacher: 'Самур Гулиевич',
+                classRoom: '2.5'
+            },
+            {
+                id: 2,
+                title: 'Web разработка',
+                teacher: 'Самур Гулиевич',
+                classRoom: '2.5'
+            },
+            {
+                id: 3,
+                title: 'история',
+                teacher: 'Джамиля Газбулаевна',
+                classRoom: '2.4'
+            },
+        ]
+    },
+    {
+        weekDay: 'Вторник',
+        classes: [
+            {
+                id: 3,
+                title: 'Русский Язык',
+                teacher: 'Зарият Исмакадиевна',
+                classRoom: '2.4'
+            },
+            {
+                id: 4,
+                title: 'киб.без ',
+                teacher: 'Ибрагим Икрамович',
+                classRoom: '3.5'
+            },
+            {
+                id: 5,
+                title: 'киб.без',
+                teacher: 'Ибрагим Икрамович',
+                classRoom: '3.5'
+            },
+        ]
+    },
+    {
+        weekDay: 'Среда',
+        classes: [
+            {
+                id: 1,
+                title: 'киб.без',
+                teacher: 'Ибрагим Икрамович',
+                classRoom: '3.5'
+            },
+            {
+                id: 2,
+                title: 'киб.без',
+                teacher: 'Ибрагим Икрамович',
+                classRoom: '3.5'
+            },
+            {
+                id: 3,
+                title: 'история',
+                teacher: 'Джамиля Газбулаевна',
+                classRoom: '2.4'
+            },
+            {
+                id: 4,
+                title: 'Web разработка',
+                teacher: 'Самур Гулиевич',
+                classRoom: '2.5'
+            },
+        ]
+    },
+
+    {
         weekDay: 'Четверг',
         classes: [
             {
